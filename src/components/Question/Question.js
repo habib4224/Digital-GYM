03e8ba794a1e925eb45e8ee22f39671ad740b6bd
habib4224 <4224habib1@gmail.com>
2022-09-29 20:06:46 +0800

    second  queastin done

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -7,7 +7,11 @@ const Question = () => {
 
             <div>
                 <h1>Question: How does react works?</h1>
-                <h4>Ans: React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</h4>
+                <h4>Answer: React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</h4>
+            </div>
+            <div>
+                <h1>Question: Difference between props and state.</h1>
+                <h4>Answer: Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</h4>
             </div>
         </div>
     );
